3d8043dcd08b24d46259acbd58da0e681a7ab941
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2021-01-23 12:47:08 +0530

    Update Hgamers.py

---
+++ Hgamers.py
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+=======
+import pygame
+import random
+import time
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+img = pygame.image.load("human.png")
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+pygame.display.set_caption('avoid covid')
+human_width = 100
+running = True
+def human(x,y):
+    game.Display.blit(img, (x, y))
+while running == True:
+    for event in pygame.event.get()
+        if event.type == pygame.quit():
+            running = False
+quit()
+pygame.quit()
+>>>>>>> parent of 6f0c60f... Merge branch 'main' of https://github.com/LearnPRG-py/Codefest-final-HGAMERS into main
