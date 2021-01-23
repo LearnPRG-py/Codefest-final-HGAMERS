31741a20eb2ea8a42063fa6fb415580e822a7201
Fossilhunter57 <77873318+Fossilhunter57@users.noreply.github.com>
2021-01-23 12:16:29 +0530

    Update Hgamers.py

--- Hgamers.py
+++ Hgamers.py
@@ -1,19 +1,10 @@
 import pygame
 import random
-import time
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-img = pygame.image.load("human.png")
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-pygame.display.set_caption('avoid covid')
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+img = pygame.image.open("human.png")
 human_width = 100
-running = True
-def human(x,y):
-    game.Display.blit(img, (x, y))
-while running == true:
-    if event.type == pygame.quit():
-        running = false:
-quit()
-pygame.quit()
+gameDisplay = pygame.display.set_mode((display))
