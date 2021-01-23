ce6e329d0c6b154a7b39e295442ad1a652890f6e
haha121506 <77873529+haha121506@users.noreply.github.com>
2021-01-23 11:57:32 +0530

    Update Hgamers.py

--- Hgamers.py
+++ Hgamers.py
@@ -1,3 +1,8 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((800,600))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+pygame.display.set_caption('avoid covid')
+human_width = 100
