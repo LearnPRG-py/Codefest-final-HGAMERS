d2127f1dcd894a771f4fe73c6695043ca59e1a35
haha121506 <77873529+haha121506@users.noreply.github.com>
2021-01-23 12:28:57 +0530

    Update Hgamers.py

--- Hgamers.py
+++ Hgamers.py
@@ -5,14 +5,16 @@ screen = pygame.display.set_mode((800,600))
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
-img = pygame.image.open("human.png")
+img = pygame.image.load("human.png")
 gameDisplay = pygame.display.set_mode((display))
 running = True
 def human(x,y):
     game.Display.blit(img, (x, y))
 while running == True:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.quit():
             running = False
+    if event.type == pygame.quit():
+        running = False
 quit()
 pygame.quit()
