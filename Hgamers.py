6217efbb51199743ad33c6ebcb2543ea51762cb9
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2021-01-23 13:28:19 +0530

    Re-doing the code just started
    importing libraries

--- Hgamers.py
+++ Hgamers.py
@@ -1,22 +1,5 @@
-import pygame
+import pygame 
 import random
+import time
 pygame.init()
-humanx = 400
-humany = 500
-human_width = 100
-screen = pygame.display.set_mode((800,600))
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-img = pygame.image.load("human.png")
-running = True
-def human(x,y):
-    screen.blit(img, (x, y))
-while running == True:
-    human(humanx, humany)
-    for Event in pygame.event.get():
-      if Event.type == pygame.QUIT:
-         running = False
-
-quit()
-pygame.quit()
+screen = pygame.display.set_mode((800, 600))
