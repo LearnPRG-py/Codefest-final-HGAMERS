94c61923ed05958aad5e8e92f882d709dead560b
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2021-01-23 13:01:00 +0530

    Update Hgamers.py

--- Hgamers.py
+++ Hgamers.py
@@ -1,21 +1,17 @@
-
 import pygame
 import random
-import time
 pygame.init()
 screen = pygame.display.set_mode((800,600))
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
 img = pygame.image.load("human.png")
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-pygame.display.set_caption('avoid covid')
-human_width = 100
 running = True
 def human(x,y):
     game.Display.blit(img, (x, y))
 while running == True:
-    for event in pygame.event.get():
-        if event.type == pygame.quit():
-            running = False
+    for Event in pygame.event.get():
+      if Event.type == pygame.QUIT:
+         running = False
 quit()
-pygame.quit()
+pygame.quit(
