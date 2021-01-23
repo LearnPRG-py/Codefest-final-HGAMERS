bec42ec2ece59291967680f576cf6e986dd56109
haha121506 <77873529+haha121506@users.noreply.github.com>
2021-01-23 12:20:22 +0530

    Update Hgamers.py

--- Hgamers.py
+++ Hgamers.py
@@ -15,5 +15,12 @@ def human(x,y):
 while running == True:
     if event.type == pygame.quit():
         running = False
+<<<<<<< Updated upstream
+=======
+while running == true:
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            running = False
+>>>>>>> Stashed changes
 quit()
 pygame.quit()
